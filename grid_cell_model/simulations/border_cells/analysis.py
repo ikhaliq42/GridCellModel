8248ec538197710738efc2946b58f2d21a57cbfe
Imran Khaliq <ikhaliq42@gmail.com>
2015-06-27 02:57:10 +0100

    Corrected error in analysis script that was producing a nearly empty rate map. Also added a script to run grid simulation without border or place cells.

--- grid_cell_model/simulations/border_cells/analysis.py
+++ grid_cell_model/simulations/border_cells/analysis.py
@@ -57,23 +57,9 @@ if ('analysis/neuron_' + str(neuron_idx) + '/rateMap') not in \
         sys.stdout.write('\rProgress: %g %%' % (progress))
         if (senders[i] == neuron_idx): spikes[j] = times[i]; j += 1
     
-    # rat position data needs to be aligned with spike times
-    print("\nAligning rat position data to spike times...")
-    length = int(sim_time/sim_dt); step = int(rat_dt/sim_dt)
-    pos_x = np.zeros(length); pos_y = np.zeros(length)
-    for i in range(len(rat_pos_x)-1):
-        progress = round(float(i)/len(rat_pos_x)*100.0,1)
-        sys.stdout.write('\rProgress: %g %%' % (progress))
-        x_mov = rat_pos_x[i+1] - rat_pos_x[i]
-        y_mov = rat_pos_y[i+1] - rat_pos_y[i]
-        for j in range(step):
-            # interpolate positions
-            pos_x[i*step + j] = rat_pos_x[i] + x_mov * j / step
-            pos_y[i*step + j] = rat_pos_y[i] + y_mov * j / step
-    
     # create a spatial rate map if one does not already exist
     print("\nGenerating spatial map...")    
-    rateMap, xedges, yedges = SNSpatialRate2DRect(spikes, pos_x, pos_y, rat_dt, 
+    rateMap, xedges, yedges = SNSpatialRate2DRect(spikes, rat_pos_x, rat_pos_y, rat_dt, 
                                         arena_dim_x, arena_dim_y, smoothingSigma)
     rateMap *= 1e3 # should be Hz
     X, Y = np.meshgrid(xedges, yedges)
@@ -85,25 +71,6 @@ else:
     rateMap = np.array(data.get(data_path + 'analysis/neuron_' +str(neuron_idx) + '/rateMap'))
     X = np.array(data.get(data_path + 'analysis/neuron_' +str(neuron_idx) + '/X'))
     Y = np.array(data.get(data_path + 'analysis/neuron_' +str(neuron_idx) + '/Y'))
-
-'''
-# create an occupancy probablity distribution for animal in arena if not already exists
-if ('/analysis/occupancy_prob_' + str(neuron_idx)) not in \
-                               data['trials'][str(trial_no)]['spikeMon_e']['events']:
-    px = occupancy_prob_dist_rect(spikes, pos_x, pos_y, 
-                                             arena_dim_x, arena_dim_y, smoothingSigma)
-else:
-    print("\Occupancy probability already exists - loading.")
-    px = np.array(data.get(data_path + 'occupancy_prob_neuron' + str(neuron_idx)))
-
-# create information specificity if not already exists
-if ('occupancy_prob_' + str(neuron_idx)) not in 
-                               data['trials'][str(trial_no)]['spikeMon_e']['events']:
-    info = information_specificity(rateMap, px)
-    sparsity = spatial_sparsity(rateMap, px)
-    out['info_specificity'] = info_e
-    out['sparsity'] = sparsity_e
-'''
 
 # Draw plot
 print("\nCreating plot...") 
